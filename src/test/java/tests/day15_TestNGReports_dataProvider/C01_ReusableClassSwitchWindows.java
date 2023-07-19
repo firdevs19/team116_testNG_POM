852c87801652d4fedd63716d2af21e9bf96f2266
firdevs19 <fiadhakeaf@gmail.com>
2023-07-19 09:38:06 +0200

    testNG

---
+++ src/test/java/tests/day15_TestNGReports_dataProvider/C01_ReusableClassSwitchWindows.java
@@ -0,0 +1,38 @@
+package tests.day15_TestNGReports_dataProvider;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C01_ReusableClassSwitchWindows {
+
+    @Test
+
+    public void switchWindowTesti(){
+
+
+        //https://the-internet.herokuapp.com/windows adresine gidin.
+        Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+
+        //Click Here butonuna basın.
+        Driver.getDriver().findElement(By.xpath("//a[text()='Click Here']")).click();
+
+        //Acilan yeni pencerenin sayfa başlığının (title) "New Window" oldugunu dogrulayin.
+        ReusableMethods.switchToWindow("New Window");
+
+        String actualTitle= Driver.getDriver().getTitle();
+        String expectedTitle="New Window";
+
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+
+        // sayfalari kapatin
+        Driver.quitDriver();
+
+
+    }
+
+}
+
